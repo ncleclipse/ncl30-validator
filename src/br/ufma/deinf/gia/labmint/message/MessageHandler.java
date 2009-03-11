c69b0bf1b0f5247957ded1cb1594db400e57deaa
roberto <roberto>
2009-03-11 18:21:14 +0000

    *** empty log message ***

--- src/br/ufma/deinf/gia/labmint/message/MessageHandler.java
+++ src/br/ufma/deinf/gia/labmint/message/MessageHandler.java
@@ -6,29 +6,32 @@ import java.util.Properties;
 
 public class MessageHandler {
 	private static MessageHandler instance = null;
-	private static Properties properties; 
-	
-	private MessageHandler(){
-	
+	private static Properties properties;
+
+	private MessageHandler() {
+
 	}
-	
-	public static MessageHandler getInstance(){
-		if(instance == null){
+
+	public static MessageHandler getInstance() {
+		if (instance == null) {
 			instance = new MessageHandler();
 			// Read properties file.
 			properties = new Properties();
 			try {
-				properties.load(new FileInputStream("messages.properties"));
+				java.net.URL url = ClassLoader
+						.getSystemResource("messages.properties");
+				properties.load(url.openStream());
+
 			} catch (IOException e) {
 			}
 		}
 		return instance;
 	}
-	
-	private static String getMessageTemplate(int msg){
+
+	private static String getMessageTemplate(int msg) {
 		return properties.getProperty((new Integer(msg)).toString());
 	}
-	
+
 	public static String getMessage(int msg) {
 		getInstance();
 		String str = MessageHandler.getMessageTemplate(msg);
