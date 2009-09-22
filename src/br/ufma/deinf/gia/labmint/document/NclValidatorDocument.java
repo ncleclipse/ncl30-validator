cbab204b1c370949ecd07475758432c47650c95e
roberto <roberto>
2009-09-22 00:53:23 +0000

    *** empty log message ***

--- src/br/ufma/deinf/gia/labmint/document/NclValidatorDocument.java
+++ src/br/ufma/deinf/gia/labmint/document/NclValidatorDocument.java
@@ -1,54 +1,25 @@
-/******************************************************************************
-Este arquivo é parte da implementação do ambiente de autoria em Nested Context
-Language - NCL Eclipse.
-
-Direitos Autorais Reservados (c) 2007-2008 UFMA/LAWS (Laboratório de Sistemas Avançados da Web) 
-
-Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob 
-os termos da Licença Pública Geral GNU versão 2 conforme publicada pela Free 
-Software Foundation.
-
-Este programa é distribuído na expectativa de que seja útil, porém, SEM 
-NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU 
-ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral do 
-GNU versão 2 para mais detalhes. 
-
-Você deve ter recebido uma cópia da Licença Pública Geral do GNU versão 2 junto 
-com este programa; se não, escreva para a Free Software Foundation, Inc., no 
-endereço 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA. 
-
-Para maiores informações:
-ncleclipse@laws.deinf.ufma.br
-http://www.laws.deinf.ufma.br/ncleclipse
-http://www.laws.deinf.ufma.br
-
-******************************************************************************
-This file is part of the authoring environment in Nested Context Language -
-NCL Eclipse.
-
-Copyright: 2007-2008 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
-
-This program is free software; you can redistribute it and/or modify it under 
-the terms of the GNU General Public License version 2 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY 
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
-details.
-
-You should have received a copy of the GNU General Public License version 2
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-
-For further information contact:
-ncleclipse@laws.deinf.ufma.br
-http://www.laws.deinf.ufma.br/ncleclipse
-http://www.laws.deinf.ufma.br
-
-*******************************************************************************/
-
-
+/*******************************************************************************
+ * This file is part of the authoring environment in Nested Context Language -
+ * NCL Eclipse.
+ * 
+ * Copyright: 2007-2009 UFMA/LAWS (Laboratory of Advanced Web Systems), All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License version 2
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * For further information contact:
+ * 		ncleclipse@laws.deinf.ufma.br
+ * 		http://www.laws.deinf.ufma.br/ncleclipse
+ * 		http://www.laws.deinf.ufma.br
+ ********************************************************************************/
 package br.ufma.deinf.gia.labmint.document;
 
 import java.io.IOException;
@@ -68,6 +39,7 @@ import org.xml.sax.SAXException;
 
 import br.ufma.deinf.gia.labmint.message.MessageList;
 import br.ufma.deinf.gia.labmint.xml.XMLParserExtend;
+import br.ufma.deinf.laws.util.DocumentUtil;
 
 public class NclValidatorDocument{
 	protected Map<String, Element> elements;
@@ -82,7 +54,7 @@ public class NclValidatorDocument{
 		this.aliases = new HashMap<String, NclValidatorDocument>();
 		this.root = doc.getDocumentElement();
 		this.path = doc.getBaseURI();
-		//NclDocumentManager.addDocument(doc, this); //registra o documento que está lendo no DocumentManager
+		//NclDocumentManager.addDocument(doc, this); //registra o documento que estah lendo no DocumentManager
 		this.id = null;
 		//JOptionPane.showMessageDialog(null, this.path);
 		System.out.println(">> Validating File = "+this.path);
@@ -161,7 +133,7 @@ public class NclValidatorDocument{
 						XMLParserExtend parser = new XMLParserExtend();
 						Document doc = null;
 						URI uri = new URI(root.getAttribute("documentURI"));
-//						Se documentUri é absoluto					
+//						Se documentUri eh absoluto					
 						if(uri.isAbsolute()) {
 							parser.parse(uri.getPath());
 							doc = parser.getDocument();
