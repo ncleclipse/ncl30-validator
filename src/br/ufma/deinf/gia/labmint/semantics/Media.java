adcf5caa61498c873b0c4f1d5ae4cad4ccf7c63e
thiago <thiago>
2010-02-19 17:47:16 +0000

    *** empty log message ***

--- src/br/ufma/deinf/gia/labmint/semantics/Media.java
+++ src/br/ufma/deinf/gia/labmint/semantics/Media.java
@@ -22,7 +22,7 @@ ncleclipse@laws.deinf.ufma.br
 http://www.laws.deinf.ufma.br/ncleclipse
 http://www.laws.deinf.ufma.br
 
-*******************************************************************************/
+ *******************************************************************************/
 
 package br.ufma.deinf.gia.labmint.semantics;
 
@@ -37,285 +37,265 @@ import br.ufma.deinf.gia.labmint.document.NclValidatorDocument;
 import br.ufma.deinf.gia.labmint.message.MessageList;
 import br.ufma.deinf.laws.util.MultiHashMap;
 
-public class Media extends ElementValidation{
+public class Media extends ElementValidation {
 
 	public Media(NclValidatorDocument doc) {
 		super(doc);
-		setTypes();		
+		setTypes();
 	}
 
 	private MultiHashMap types = null;
 	private String idMedia = null;
-	
-    public boolean validate(Element eMedia){
-        boolean resultado = true;
-        
-        idMedia = eMedia.getAttribute("id");
-        
-        //Verifica se o atributo 'descriptor' da <media> aponta para um <descriptor>.
-        if(!hasValidMediaDescriptorAttribute(eMedia)) resultado = false;
 
-		//Verifica se o atributo 'type' da <media> possui um valor valido.
-        if(!hasValidMediaTypeAttribute(eMedia)) resultado = false;
+	public boolean validate(Element eMedia) {
+		boolean resultado = true;
 
-		//Verifica se o atributo 'src' da <media> possui um valor valido.
-        if(!hasValidMediaSrcAttribute(eMedia)) resultado = false;
+		idMedia = eMedia.getAttribute("id");
 
-        //
-        //if(!hasValidMediaIDAttribute(eMedia)) resultado = false;
+		// Verifica se o atributo 'descriptor' da <media> aponta para um
+		// <descriptor>.
+		if (!hasValidMediaDescriptorAttribute(eMedia))
+			resultado = false;
 
-        //Verifica se o atributo 'refer' da <media> aponta para um outro elemento <media>.
-        if(!hasValidMediaReferAttribute(eMedia)) resultado = false;
+		// Verifica se o atributo 'type' da <media> possui um valor valido.
+		if (!hasValidMediaTypeAttribute(eMedia))
+			resultado = false;
 
-		//Verifica se o atributo 'newInstace' da <media> possui um valor valido.
-        //if(!hasValidMediaNewInstanceAttribute(eMedia)) resultado = false;
-        
-        //Verifica se o arquivo especificado no atributo 'src' tem uma extensao valida.
-        //if (!hasValidExtension(eMedia)) resultado = false;
-        
-        //Verifica se a extensao diverge da definida pelo atributo type.
-        if (!hasValidExtensionForMediaType(eMedia)) resultado = false;
+		// Verifica se o atributo 'src' da <media> possui um valor valido.
+		if (!hasValidMediaSrcAttribute(eMedia))
+			resultado = false;
 
-        //Atributo refer não deve ser utilizado com o src simultaneamente
-        if(!eMedia.hasAttribute("refer")){
-        	//Atributo type é obrigatório se src não for definido        	
-        	if(!eMedia.hasAttribute("src")){
-        		if(!eMedia.hasAttribute("type")){
-        			MessageList.addError(doc.getId(), 4108, eMedia);
-        			resultado = false;
-        		}
-        	}
-        }
-        else if(eMedia.hasAttribute("src")){
-        	MessageList.addWarning(doc.getId(), 4107, eMedia);
-    		resultado = false;
-        }
-        	
-        return resultado;
-    }
+		//
+		// if(!hasValidMediaIDAttribute(eMedia)) resultado = false;
 
-    private boolean hasValidMediaDescriptorAttribute(Element eMedia){
-    	if( eMedia.hasAttribute("descriptor") ) {
+		// Verifica se o atributo 'refer' da <media> aponta para um outro
+		// elemento <media>.
+		if (!hasValidMediaReferAttribute(eMedia))
+			resultado = false;
+
+		// Verifica se o atributo 'newInstace' da <media> possui um valor
+		// valido.
+		// if(!hasValidMediaNewInstanceAttribute(eMedia)) resultado = false;
+
+		// Verifica se o arquivo especificado no atributo 'src' tem uma extensao
+		// valida.
+		// if (!hasValidExtension(eMedia)) resultado = false;
+
+		// Verifica se a extensao diverge da definida pelo atributo type.
+		if (!hasValidExtensionForMediaType(eMedia))
+			resultado = false;
+
+		// Atributo refer não deve ser utilizado com o src simultaneamente
+		if (!eMedia.hasAttribute("refer")) {
+			// Atributo type é obrigatório se src não for definido
+			if (!eMedia.hasAttribute("src")) {
+				if (!eMedia.hasAttribute("type")) {
+					MessageList.addError(doc.getId(), 4108, eMedia);
+					resultado = false;
+				}
+			}
+		} else if (eMedia.hasAttribute("src")) {
+			MessageList.addWarning(doc.getId(), 4107, eMedia);
+			resultado = false;
+		}
+
+		return resultado;
+	}
+
+	private boolean hasValidMediaDescriptorAttribute(Element eMedia) {
+		if (eMedia.hasAttribute("descriptor")) {
 			String idDescriptor = eMedia.getAttribute("descriptor");
 			Element element = doc.getElement(idDescriptor);
-			if( element==null ) {
-				Vector <String> args = new Vector <String>();
-    			args.add(idMedia);
-				MessageList.addError(doc.getId(), 
-						4106,
-				   		eMedia, args);
+			if (element == null) {
+				Vector<String> args = new Vector<String>();
+				args.add(idMedia);
+				MessageList.addError(doc.getId(), 4106, eMedia, args);
+				return false;
+			} else if (element.getTagName().compareTo("descriptor") != 0
+					&& element.getTagName().compareTo("descriptorSwitch") != 0) {
+				Vector<String> args = new Vector<String>();
+				args.add(idMedia);
+				MessageList.addError(doc.getId(), 4106, eMedia, args);
 				return false;
 			}
-			else if( element.getTagName().compareTo("descriptor")!=0 &&
-					element.getTagName().compareTo("descriptorSwitch")!=0) {
-				Vector <String> args = new Vector <String>();
-    			args.add(idMedia);
-				MessageList.addError(doc.getId(), 
-						4106,
-				   		eMedia, args);
-				return false;				
-			}
 		}
-        return true;
-    }
+		return true;
+	}
 
-    private boolean hasValidMediaTypeAttribute(Element eMedia){
-    	if (eMedia.hasAttribute("type")){
-    	
-    		String type = eMedia.getAttribute("type");
-    		
-    		if (!types.containsKey(type)){
-    			Vector <String> args = new Vector <String>();
-    			args.add(type);
-    			MessageList.addWarning(doc.getId(), 
-						4105,
-				   		eMedia, args);
-    			return false;
-    		}
-    	}else if (!eMedia.hasAttribute("src") && !eMedia.hasAttribute("refer")){
-    		MessageList.addError(doc.getId(), 
-					4104,
-			   		eMedia);
+	private boolean hasValidMediaTypeAttribute(Element eMedia) {
+		if (eMedia.hasAttribute("type")) {
+
+			String type = eMedia.getAttribute("type");
+
+			if (!types.containsKey(type)) {
+				Vector<String> args = new Vector<String>();
+				args.add(type);
+				MessageList.addWarning(doc.getId(), 4105, eMedia, args);
+				return false;
+			}
+		} else if (!eMedia.hasAttribute("src") && !eMedia.hasAttribute("refer")) {
+			MessageList.addError(doc.getId(), 4104, eMedia);
 			return false;
-    	}
-        return true;
-    }
+		}
+		return true;
+	}
 
-    private boolean hasValidMediaSrcAttribute(Element eMedia){
-        if (eMedia.hasAttribute("src"))
-        {
-            String src = eMedia.getAttribute("src");
-           
-            File fMedia;
-           
-           
-                fMedia = new File(src);
-               
-                //System.out.println("br.ufma.deinf.gia.labmint.semantics.Media hasValidMediaSrcAttribute uri=" + uri);
-                if(fMedia.isFile())
-                    return true;
-                else{
-                    String absolute = doc.getDir().toString()+src;
-                 //   System.out.println(absolute.substring(7));
-                    fMedia = new File(absolute.substring(7));
-                    //System.out.println((doc.getDir()+src));
-                    //System.out.println("br.ufma.deinf.gia.labmint.semantics.Media hasValidMediaSrcAttribute uri=" + uri);
-                    if(!fMedia.isFile()){
-                        Vector <String> args = new Vector <String>();
-                        args.add(src);
-                        MessageList.addWarning(doc.getId(),
-                                4103,
-                                eMedia, args);
-                        return false;
-                    }
-                   
-                }
-               
-           
-        }
-        return true;
-    }
+	private boolean hasValidMediaSrcAttribute(Element eMedia) {
+		if (eMedia.hasAttribute("src")) {
+			String src = eMedia.getAttribute("src");
 
-    private boolean hasValidMediaIDAttribute(Element eMedia){
-        //TODO Validar Atributo id
-        return true;
-    }
+			File fMedia;
 
-    private boolean hasValidMediaReferAttribute(Element eMedia){
-    	if( eMedia.hasAttribute("refer") ) {
-			
+			fMedia = new File(src);
+
+			if (fMedia.isFile())
+				return true;
+			else {
+				String absolute = doc.getDir().toString() + src;
+
+				fMedia = new File(absolute.substring(7));
+
+				if (!fMedia.isFile()) {
+					Vector<String> args = new Vector<String>();
+					args.add(src);
+					MessageList.addWarning(doc.getId(), 4103, eMedia, args);
+					return false;
+				}
+
+			}
+
+		}
+		return true;
+	}
+
+	private boolean hasValidMediaIDAttribute(Element eMedia) {
+		// TODO Validar Atributo id
+		return true;
+	}
+
+	private boolean hasValidMediaReferAttribute(Element eMedia) {
+		if (eMedia.hasAttribute("refer")) {
+
 			String idRefer = eMedia.getAttribute("refer");
 			Element element = doc.getElement(idRefer);
-			if(element==null) {
-				Vector <String> args = new Vector <String>();
-    			args.add(idRefer);
-    			MessageList.addWarning(doc.getId(), 
-						4102,
-						eMedia, args);				
-				return false;				
-			}
-			else if(element.getTagName().compareTo("media")!=0) {
-				Vector <String> args = new Vector <String>();
-    			args.add(idRefer);
-    			MessageList.addWarning(doc.getId(), 
-						4102,
-						eMedia, args);				
+			if (element == null) {
+				Vector<String> args = new Vector<String>();
+				args.add(idRefer);
+				MessageList.addWarning(doc.getId(), 4102, eMedia, args);
 				return false;
-			} else if(element.hasAttribute("refer")){
-				//cannot refer a media that has a refer too
-				Vector <String> args = new Vector <String>();
-    			args.add(idRefer);
-    			MessageList.addWarning(doc.getId(), 
-						4109,
-						eMedia, args);				
+			} else if (element.getTagName().compareTo("media") != 0) {
+				Vector<String> args = new Vector<String>();
+				args.add(idRefer);
+				MessageList.addWarning(doc.getId(), 4102, eMedia, args);
+				return false;
+			} else if (element.hasAttribute("refer")) {
+				// cannot refer a media that has a refer too
+				Vector<String> args = new Vector<String>();
+				args.add(idRefer);
+				MessageList.addWarning(doc.getId(), 4109, eMedia, args);
 				return false;
 			}
-		}		
-        return true;
-    }
-
-    private boolean hasValidMediaNewInstanceAttribute(Element eMedia){
-        //TODO Validar Atributo newIsntance
-        return true;
-    }
-    
-    //FIX: fix this function
-    private boolean hasValidExtension(Element eMedia){
-    	/*
-    	if (eMedia.hasAttribute("src"))
-    	{
-    		setTypes();
-    		
-    		String src = eMedia.getAttribute("src");
-    		String extension = getExtension(src);
-    		
-    		if (!types.containsValue(extension)){
-    			MessageList.addError(documentId, 
-						"Invalid extension for <media> "+ idMedia + ".",
-						eMedia);
-				return false;
-    		}
-    		
-    	}
-    	*/
-    	return true;
-    }
-    
-    private boolean hasValidExtensionForMediaType(Element eMedia){
-    	if (eMedia.hasAttribute("src") && eMedia.hasAttribute("type")){
-    		
-    		String type = eMedia.getAttribute("type");
-    		String src = eMedia.getAttribute("src");
-    		String extension = getExtension(src);
-    		if (!types.containsKey(type) || !types.get(type).contains(extension)){
-    			Vector <String> args = new Vector <String>();
-    			args.add(type);
-    			args.add(idMedia);
-    			MessageList.addWarning(doc.getId(), 
-						4101,
-						eMedia, args);
-    		}
-    	}    	
-    	return true;
-    }
-    
-    private  String getExtension(String src){
-    	String extension = "";
-    	for (int i=src.length()-1;i>=0;i--) { 
-			if (src.charAt(i) == '.') break;
-    		extension = src.charAt(i) + extension;
 		}
-    	return extension;
-    }
-    
-    private void setTypes(){
-    	if (types == null){
-    		types = new MultiHashMap();
-    	
-    		types.put("text/html", "html");
-    		types.put("text/html", "htm");
-    		
-    		types.put("text/css", "css");
-    		
-    		types.put("text/xml", "xml");
-    		
-    		types.put("image/bmp", "bmp");
-    		types.put("image/png", "png");
-    		types.put("image/mng", "mng");
-        	types.put("image/gif", "gif");
-        	types.put("image/jpeg", "jpg");
-        	types.put("image/jpeg", "jpeg");
-        	//types.put("image/mpeg", "");
-        	
-        	types.put("audio/basic", "wav");
-        	//types.put("audio/ac3", "ac3"); 
-        	types.put("audio/mp3", "mp3");
-        	types.put("audio/mp2", "mp2");
-        	types.put("audio/mpeg", "mpeg");
-        	types.put("audio/mpeg", "mpg");
-        	types.put("audio/mpeg4", "mp4");
-        	types.put("audio/mpeg4", "mpg4");
-        	
-        	types.put("video/mpeg", "mpeg");
-        	types.put("video/mpeg", "mpg");
-        	
-        	types.put("application/x-ginga-NCL", "ncl");
-        	types.put("application/x-ncl-NCL", "ncl");
-        	
-        	types.put("application/x-ginga-NCLua", "lua");
-        	types.put("application/x-ncl-NCLua", "lua");
-        	
-        	types.put("application/x-ginga-NCLet", "class");
-        	types.put("application/x-ncl-NCLet", "class");
-        	types.put("application/x-ginga-NCLet", "jar");
-        	types.put("application/x-ncl-NCLet", "jar");
-        	
-        	types.put("application/x-ginga-settings", "");
-        	types.put("application/x-ncl-settings", "");
-        	
-        	types.put("application/x-ginga-time", "");
-        	types.put("application/x-ncl-time", "");
-    	}
-    }
+		return true;
+	}
+
+	private boolean hasValidMediaNewInstanceAttribute(Element eMedia) {
+		// TODO Validar Atributo newIsntance
+		return true;
+	}
+
+	// FIX: fix this function
+	private boolean hasValidExtension(Element eMedia) {
+		/*
+		 * if (eMedia.hasAttribute("src")) { setTypes();
+		 * 
+		 * String src = eMedia.getAttribute("src"); String extension =
+		 * getExtension(src);
+		 * 
+		 * if (!types.containsValue(extension)){
+		 * MessageList.addError(documentId, "Invalid extension for <media> "+
+		 * idMedia + ".", eMedia); return false; }
+		 * 
+		 * }
+		 */
+		return true;
+	}
+
+	private boolean hasValidExtensionForMediaType(Element eMedia) {
+		if (eMedia.hasAttribute("src") && eMedia.hasAttribute("type")) {
+
+			String type = eMedia.getAttribute("type");
+			String src = eMedia.getAttribute("src");
+			String extension = getExtension(src);
+			if (!types.containsKey(type)
+					|| !types.get(type).contains(extension)) {
+				Vector<String> args = new Vector<String>();
+				args.add(type);
+				args.add(idMedia);
+				MessageList.addWarning(doc.getId(), 4101, eMedia, args);
+			}
+		}
+		return true;
+	}
+
+	private String getExtension(String src) {
+		String extension = "";
+		for (int i = src.length() - 1; i >= 0; i--) {
+			if (src.charAt(i) == '.')
+				break;
+			extension = src.charAt(i) + extension;
+		}
+		return extension;
+	}
+
+	private void setTypes() {
+		if (types == null) {
+			types = new MultiHashMap();
+
+			types.put("text/html", "html");
+			types.put("text/html", "htm");
+
+			types.put("text/css", "css");
+
+			types.put("text/xml", "xml");
+
+			types.put("image/bmp", "bmp");
+			types.put("image/png", "png");
+			types.put("image/mng", "mng");
+			types.put("image/gif", "gif");
+			types.put("image/jpeg", "jpg");
+			types.put("image/jpeg", "jpeg");
+			// types.put("image/mpeg", "");
+
+			types.put("audio/basic", "wav");
+			// types.put("audio/ac3", "ac3");
+			types.put("audio/mp3", "mp3");
+			types.put("audio/mp2", "mp2");
+			types.put("audio/mpeg", "mpeg");
+			types.put("audio/mpeg", "mpg");
+			types.put("audio/mpeg4", "mp4");
+			types.put("audio/mpeg4", "mpg4");
+
+			types.put("video/mpeg", "mpeg");
+			types.put("video/mpeg", "mpg");
+
+			types.put("application/x-ginga-NCL", "ncl");
+			types.put("application/x-ncl-NCL", "ncl");
+
+			types.put("application/x-ginga-NCLua", "lua");
+			types.put("application/x-ncl-NCLua", "lua");
+
+			types.put("application/x-ginga-NCLet", "class");
+			types.put("application/x-ncl-NCLet", "class");
+			types.put("application/x-ginga-NCLet", "jar");
+			types.put("application/x-ncl-NCLet", "jar");
+
+			types.put("application/x-ginga-settings", "");
+			types.put("application/x-ncl-settings", "");
+
+			types.put("application/x-ginga-time", "");
+			types.put("application/x-ncl-time", "");
+		}
+	}
 
 }
