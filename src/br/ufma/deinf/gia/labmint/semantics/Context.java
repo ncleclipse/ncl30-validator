76d55130116503ec536cebbdbaa5debb252b4658
roberto <roberto>
2012-06-02 01:27:24 +0000

    Fix character codification.

--- src/br/ufma/deinf/gia/labmint/semantics/Context.java
+++ src/br/ufma/deinf/gia/labmint/semantics/Context.java
@@ -1,22 +1,22 @@
 /*******************************************************************************
- * Este arquivo Ã© parte da implementaÃ§Ã£o do ambiente de autoria em Nested 
+ * Este arquivo é parte da implementação do ambiente de autoria em Nested 
  * Context Language - NCL Eclipse.
- * Direitos Autorais Reservados (c) 2007-2010 UFMA/LAWS (LaboratÃ³rio de Sistemas 
- * AvanÃ§ados da Web)
+ * Direitos Autorais Reservados (c) 2007-2010 UFMA/LAWS (Laboratório de Sistemas 
+ * Avançados da Web)
  *
- * Este programa Ã© software livre; vocÃª pode redistribuÃ­-lo e/ou modificÃ¡-lo sob
- * os termos da LicenÃ§a PÃºblica Geral GNU versÃ£o 2 conforme publicada pela Free 
+ * Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo sob
+ * os termos da Licença Pública Geral GNU versão 2 conforme publicada pela Free 
  * Software Foundation.
  *
- * Este programa Ã© distribuÃ­do na expectativa de que seja Ãºtil, porÃ©m, SEM 
- * NENHUMA GARANTIA; nem mesmo a garantia implÃ­cita de COMERCIABILIDADE OU
- * ADEQUAÃ‡ÃƒO A UMA FINALIDADE ESPECÃ�FICA. Consulte a LicenÃ§a PÃºblica Geral do
- * GNU versÃ£o 2 para mais detalhes. VocÃª deve ter recebido uma cÃ³pia da LicenÃ§a
- * PÃºblica Geral do GNU versÃ£o 2 junto com este programa; se nÃ£o, escreva para a
- * Free Software Foundation, Inc., no endereÃ§o 59 Temple Street, Suite 330,
+ * Este programa é distribuído na expectativa de que seja útil, porém, SEM 
+ * NENHUMA GARANTIA; nem mesmo a garantia implícita de COMERCIABILIDADE OU
+ * ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA. Consulte a Licença Pública Geral do
+ * GNU versão 2 para mais detalhes. Você deve ter recebido uma cópia da Licença
+ * Pública Geral do GNU versão 2 junto com este programa; se não, escreva para a
+ * Free Software Foundation, Inc., no endereço 59 Temple Street, Suite 330,
  * Boston, MA 02111-1307 USA.
  *
- * Para maiores informaÃ§Ãµes:
+ * Para maiores informações:
  * - ncleclipse@laws.deinf.ufma.br
  * - http://www.laws.deinf.ufma.br/ncleclipse
  * - http://www.laws.deinf.ufma.br
